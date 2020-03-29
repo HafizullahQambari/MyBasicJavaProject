827cbb94ef0b55a2365d8f932a83ff2e4c7b47d1
HafizullahQambari <hafiz.kabul@gmail.com>
2020-03-29 15:03:20 -0400

    inheritance codes added

---
+++ src/com/syntax/class19/TestClass.java
@@ -0,0 +1,37 @@
+package com.syntax.class19;
+
+public class TestClass {
+		public static void main(String[] args) {
+			
+			Dog dog=new Dog();
+			//access variable from own class
+			dog.breed="Husky";
+			// access variables form parent class
+			dog.color="Grey";
+			dog.fur="Too Much";
+			dog.size="Big";
+		// access methods form oown class
+			dog.bark();
+			// access methods from another class
+			dog.beWild();
+			dog.eat();
+			dog.sleep();
+			dog.age=2;
+			dog.display();
+			
+			Animals animals=new Animals();
+			animals.color="Any";
+			animals.size="Any";
+			animals.fur="Any";
+			
+			animals.sleep();
+			animals.eat();
+			animals.beWild();
+			
+			
+			
+			
+			
+	
+}
+}
---
+++ src/com/syntax/class19/childCat.java
@@ -0,0 +1,16 @@
+package com.syntax.class19;
+
+public class childCat extends Cat {
+	
+	public static void main(String[] args) {
+		Cat obj=new Cat();
+		obj.color="Black";
+		obj.size="Big";
+		obj.tail="yes";
+		
+		obj.eat();
+		obj.sleep();
+		
+	}
+
+}
